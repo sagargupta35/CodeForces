35e57979d98a7a851022afaab032f58b169af04a
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2023-11-25 22:55:35 +0530

    Create Chip_and_Ribbon.java
    probelm 1 - 25/11/2023 - arrays

---
+++ Chip_and_Ribbon.java
@@ -0,0 +1,86 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.function.*;
+
+public class CodeChef {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0){
+            int n = in.nextInt();
+            int [] arr = new int [n];
+            for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+            long sum = arr[0]-1;
+            for(int i = 1; i<n; i++){
+                if(arr[i] > arr[i-1]){
+                    sum += arr[i]-arr[i-1];
+                }
+            }
+            out.println(sum);
+        }
+        out.close();
+    }
+}
