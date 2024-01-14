e1e60b6fa099ac3c9bccc25526335e64c9bf7f40
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-01-14 18:34:11 +0530

    Create Deja_Vu.java
    problem 1 - 14-01-2024 - arrays and math

---
+++ Deja_Vu.java
@@ -0,0 +1,131 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+import java.util.function.*;
+import java.util.regex.Pattern;
+
+public class CodeChef2{
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    static FastReader in = new FastReader();
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static long fastPower(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static int mod = (int) Math.pow(10, 9) + 7;
+
+    public static void main (String[] args) throws java.lang.Exception {
+        FastWriter out = new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0) {
+            int n = in.nextInt();
+            int q = in.nextInt();
+            int [] a = ai(n);
+            int [] x = ai(q);
+            int min;
+            int i = 0, max;
+            while(i < q){
+                min = x[i];
+                max = (int) fastPower(2, min);
+                int max1 = max>>1;
+                for(int j = 0; j<n; j++){
+                    if(a[j]%max == 0){
+                        a[j] += max1;
+                    }
+                }
+                i++;
+                while(i < q && x[i] >= min)i++;
+            }
+            for(int j = 0; j < n; j++) out.print(a[j] + " ");
+            out.println("");
+        }
+        out.close();
+    }
