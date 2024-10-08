f8cea2e4156ecc33a7b9fcc6cb84de6891f694df
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-10-08 12:44:58 +0530

    Create Berland_Fair.java
    problem 3 - 08-10-2024 - greedy and brute force

---
+++ Berland_Fair.java
@@ -0,0 +1,114 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        int n = in.nextInt();
+        long t = in.nextLong();
+        int [] arr = ai(n);
+        long sum = 0;
+        while (true){
+            long cur = 0;
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                if(cur + arr[i] <= t){
+                    cur += arr[i];
+                    cnt++;
+                }
+            }
+            if(cur == 0) break;
+            sum += (t/cur)*cnt;
+            t %= cur;
+        }
+        System.out.println(sum);
+    }
+
+
+    static class Pair<T, U> {
+        T f;
+        U s;
+
+        public Pair(T first, U sec){
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+    }
+
+    static int mod = (int) 1e9 + 7;
+
+    static int aMax(int [] arr){
+        int max = Integer.MIN_VALUE;
+        for(int j : arr) max = Math.max(max,j);
+        return max;
+    }
+
+    static int aMin(int [] arr){
+        int min = Integer.MAX_VALUE;
+        for(int j : arr) min = Math.min(min, j);
+        return min;
+    }
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
