2c233643d8f40a757dcd1af48c131f645b026d41
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-10-25 14:43:56 +0530

    Create Neutral_Tonality.java
    problem 3 - 25-10-2024 - greedy, sortings and two pointers

---
+++ Neutral_Tonality.java
@@ -0,0 +1,158 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+    public static void main(String[] args) throws java.lang.Exception {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int[] arr = ai(n);
+            int[] b = ai(m);
+            Arrays.sort(b);
+            reverse(b);
+            int [] res = new int [n+m];
+            int [] suf = new int [n];
+            suf[n-1] = arr[n-1];
+            for (int i = n - 2; i >= 0; i--) {
+                suf[i] = Math.max(suf[i+1], arr[i]);
+            }
+            int cura = 0, curb = 0, next = 0;
+            while (next < res.length){
+                if(cura == n || (curb < m && suf[cura] <= b[curb])){
+                    res[next++] = b[curb++];
+                } else res[next++] = arr[cura++];
+            }
+            for (int re : res) {
+                System.out.print(re + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void reverse(int [] arr){
+        int i = 0, j = arr.length-1;
+        while (i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class  Triplet<T>{
+        T x, y, z;
+
+        public Triplet(T x, T y, T z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    static Triplet<Long> extendedEuclid(long a, long b){ // a>b
+        if(b == 0){
+            return new Triplet<>(a, 0L, a);
+        }
+
+        Triplet<Long> smallAns = extendedEuclid(b, a%b);
+        long y = smallAns.x - (a/b)*smallAns.y;
+        return new Triplet<>(smallAns.y, y, smallAns.z);
+
+    }
+
+    static long modularMultiplicativeInverse(long a, long m){
+        long gcd = gcd(a, m);
+        if(gcd != 1){
+            return -1;
+        }
+        long x = extendedEuclid(a, m).x;
+        x = (x%m + m)%m;
+        return x;
+    }
+
+    static long nCrModPFermat(int n, int r, int p, long [] fac) {
+        if (n<r) return 0;
+        if (r == 0) return 1;
+
+        return ((fac[n] * modularMultiplicativeInverse(fac[r], p)) % p *
+                (modularMultiplicativeInverse(fac[n - r], p)) % p) % p;
+    }
+
+    static class Pair<T, U> {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
