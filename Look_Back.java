9777bce3e4818b0baa34d129d79a31c4b238a1c0
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-10-27 08:08:46 +0530

    Create Look_Back.java
    problem 1 - 27-10-2024 - dp

---
+++ Look_Back.java
@@ -0,0 +1,159 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) throws java.lang.Exception {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int[] arr = ai(n);
+            int log = (int) (Math.log(arr[0])/Math.log(2));
+            long sum = 0;
+            for (int i = 1; i < n; i++) {
+                int l = (int) (Math.log(arr[i])/Math.log(2));
+                if(l > log){
+                    log = l;
+                } else{
+                    if(f(arr[i-1], arr[i])){
+                        log++;
+                    }
+                    sum += log - l;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+
+    static boolean f(int pre, int cur){
+        int l1 = (int) (Math.log(pre)/Math.log(2));
+        int l2 = (int) (Math.log(cur)/Math.log(2));
+        if(l1 < l2){
+            pre *= (1 << (l2 - l1));
+        } else if(l2 < l1){
+            cur *= (1 << (l1 - l2));
+        }
+        return pre > cur;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class  Triplet<T>{
+        T x, y, z;
+
+        public Triplet(T x, T y, T z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    static Triplet<Long> extendedEuclid(long a, long b){ // a>b
+        if(b == 0){
+            return new Triplet<>(a, 0L, a);
+        }
+
+        Triplet<Long> smallAns = extendedEuclid(b, a%b);
+        long y = smallAns.x - (a/b)*smallAns.y;
+        return new Triplet<>(smallAns.y, y, smallAns.z);
+
+    }
+
+    static long modularMultiplicativeInverse(long a, long m){
+        long gcd = gcd(a, m);
+        if(gcd != 1){
+            return -1;
+        }
+        long x = extendedEuclid(a, m).x;
+        x = (x%m + m)%m;
+        return x;
+    }
+
+    static long nCrModPFermat(int n, int r, int p, long [] fac) {
+        if (n<r) return 0;
+        if (r == 0) return 1;
+
+        return ((fac[n] * modularMultiplicativeInverse(fac[r], p)) % p *
+                (modularMultiplicativeInverse(fac[n - r], p)) % p) % p;
+    }
+
+    static class Pair<T, U> {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
