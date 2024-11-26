1fd45b1dc9822adcba7842ea63783ab78519afe3
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-11-26 21:58:01 +0530

    Create Fight_with_Monsters.java
    problem 1 - 26-11-2024 - greedy and math

---
+++ Fight_with_Monsters.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.Function;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int k = in.nextInt();
+        int[] arr = ai(n);
+        for (int i = 0; i < n; i++) {
+            if(arr[i] % (a+b) == 0){
+                arr[i] = b+a;
+            } else arr[i] %= (a+b);
+            arr[i] = (arr[i]-1)/a;
+        }
+        Arrays.sort(arr);
+        int sum = 0;
+        for (int i = 0; i < n && arr[i] <= k; i++) {
+            sum++;
+            k -= arr[i];
+        }
+        System.out.println(sum);
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U> {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
