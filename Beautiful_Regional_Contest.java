8a5817628dd3ae19fff374722971280aa98ffd0a
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-12-03 16:47:02 +0530

    Create Beautiful_Regional_Contest.java
    problem - 4 - 03-12-2024 - brute force and greedy

---
+++ Beautiful_Regional_Contest.java
@@ -0,0 +1,144 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int[] arr = ai(n);
+            if(n < 10){
+                System.out.println("0 0 0");
+                continue;
+            }
+            int g = 0, s = 1, b = 0;
+            int i = 0;
+            while (i < n && arr[i] == arr[0]){
+                g++;
+                i++;
+            }
+            if(i >= n/2){
+                System.out.println("0 0 0");
+                continue;
+            }
+            while (i < n && (s <= g || (i < n-1 && arr[i] == arr[i+1]))){
+                s++;
+                i++;
+            }
+            if(i >= n/2 - 1){
+                System.out.println("0 0 0");
+                continue;
+            }
+            int j = n/2 - 1;
+            while (arr[j] == arr[j+1]) j--;
+            if(j <= i || (j - i + 1 <= g)) System.out.println("0 0 0");
+            else{
+                System.out.println(g + " " + s + " " + (j - i));
+            }
+        }
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U>  {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(!(obj instanceof Pair<?,?>)) return false;
+            Pair<T, U> pair = (Pair<T, U>) obj;
+            return f.equals(pair.f) && s.equals(pair.s);
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
