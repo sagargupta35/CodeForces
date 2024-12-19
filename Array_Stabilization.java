5dd29eeea09b4c6f8d235fb7703a38d327feac7e
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-12-19 09:50:26 +0530

    Create Array_Stabilization.java
    problem 1 - 19-12-2024 - dfs

---
+++ Array_Stabilization.java
@@ -0,0 +1,146 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int d = in.nextInt();
+            int[] arr = ai(n);
+            int [] minDist = new int [n];
+            Arrays.fill(minDist, -1);
+            boolean [] vis = new boolean[n];
+            boolean flag = true;
+            for (int i = 0; i < n; i++) {
+                if(!vis[i]){
+                    int j = findMinDist(arr, minDist, i, d, vis);
+                    if(j == -1){
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if(!flag){
+                System.out.println(-1);
+                continue;
+            }
+            int max = 0;
+            for(int j : minDist) max = Math.max(max, j);
+            System.out.println(max);
+        }
+    }
+
+    static int findMinDist(int [] arr, int [] minDist, int st, int d, boolean [] vis){
+        if(vis[st]) return minDist[st];
+        vis[st] = true;
+        if(arr[st] == 0){
+            return minDist[st] = 0;
+        }
+        int j = findMinDist(arr, minDist, (st+d)%arr.length, d, vis);
+        if(j == -1) return -1;
+        return minDist[st] = ++j;
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U>  {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(!(obj instanceof Pair<?,?>)) return false;
+            Pair<T, U> pair = (Pair<T, U>) obj;
+            return f.equals(pair.f) && s.equals(pair.s);
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
