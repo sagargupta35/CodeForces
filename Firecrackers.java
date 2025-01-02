7c1ea3bff9d2a6f6f38c3839d5ac5c70392d77bf
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2025-01-02 09:59:42 +0530

    Create Firecrackers.java
    problem 1 - 02-01-2025 - binary search

---
+++ Firecrackers.java
@@ -0,0 +1,144 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int[] arr = ai(m);
+            Arrays.sort(arr);
+            int lo = 0, hi = m-1, ans = 0;
+            while (lo <= hi){
+                int mid = lo + (hi - lo)/2;
+                if(check(arr, mid, a, b, n)){
+                    ans = mid+1;
+                    lo = mid+1;
+                } else hi = mid-1;
+            }
+            System.out.println(ans);
+        }
+    }
+
+    static boolean check(int [] arr, int mid, int a, int b, int n){
+        int can = Math.abs(b - a) - 1;
+        if(can < (mid + 1)) return false;
+        int max = -1;
+        for (int i = mid, j = 0; i >= 0; i--, j++) {
+            max = Math.max(max, arr[i] + j);
+        }
+        int time;
+        if(a < b){
+            time = b - 2;
+        } else{
+            time = n - b - 1;
+        }
+        return time >= max;
+
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U>  {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(!(obj instanceof Pair<?,?>)) return false;
+            Pair<T, U> pair = (Pair<T, U>) obj;
+            return f.equals(pair.f) && s.equals(pair.s);
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
