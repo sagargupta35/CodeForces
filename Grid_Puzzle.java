29a3bae07871a0014bca11a2e973170a35e23ecf
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2025-01-08 10:46:28 +0530

    Create Grid_Puzzle.java
    problem 2 - 08-01-2025 - constructive

---
+++ Grid_Puzzle.java
@@ -0,0 +1,138 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int[] arr = ai(n);
+            int ops = 0;
+            int level = 0;
+            for (int i = 0; i < n; i++) {
+                if(arr[i] <= 0){
+                    level = 0;
+                    continue;
+                }
+                ops++;
+                if(arr[i] <= 2){
+                    if(i == n-1) continue;
+                    if(level == 0){
+                        arr[i+1] -= 2;
+                        level++;
+                    } else{
+                        level = 0;
+                        if(arr[i+1] == 3 || arr[i+1] == 4){
+                            arr[i+1] = 2;
+                        }
+                    }
+                } else{
+                    level = 0;
+                }
+            }
+            System.out.println(ops);
+        }
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U>  {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(!(obj instanceof Pair<?,?>)) return false;
+            Pair<T, U> pair = (Pair<T, U>) obj;
+            return f.equals(pair.f) && s.equals(pair.s);
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
