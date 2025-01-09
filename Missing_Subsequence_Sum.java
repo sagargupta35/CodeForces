7d73968a244e7cd0867657949a36291910d21651
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2025-01-09 19:42:38 +0530

    Create Missing_Subsequence_Sum.java
    problem 3 - 09-01-2025 - math and implementation

---
+++ Missing_Subsequence_Sum.java
@@ -0,0 +1,140 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0){
+            int n = in.nextInt();
+            int k = in.nextInt();
+            List<Integer> list = new ArrayList<>();
+            int pow = 1;
+            while (pow*2 <= k) pow *= 2;
+            for (int i = 1; i <= n; i *= 2) {
+                if(i != pow){
+                    list.add(i);
+                }
+            }
+            if(k+1 <= n) list.add(k+1);
+            if(k != pow) list.add(k-pow);
+            if((pow | (pow*2)) <= n) list.add(pow | (pow*2));
+            System.out.println(list.size());
+            for(int j : list) System.out.print(j + " ");
+            System.out.println();
+        }
+    }
+
+    static int binarySearch(List<Integer> list, int target){
+        int lo = 0, hi = list.size()-1, ans = -1;
+        while (lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(list.get(mid) < target){
+                ans = list.get(mid);
+                lo = mid+1;
+            } else hi = mid-1;
+        }
+        return ans;
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U>  {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(!(obj instanceof Pair<?,?>)) return false;
+            Pair<T, U> pair = (Pair<T, U>) obj;
+            return f.equals(pair.f) && s.equals(pair.s);
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
