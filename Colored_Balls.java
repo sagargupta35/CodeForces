99cde397b7b4aa85e7347ad9c521b5dfc3931fe0
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2025-01-10 14:05:47 +0530

    Create Colored_Balls.java
    problem 2 - 10-01-2025 - dp

---
+++ Colored_Balls.java
@@ -0,0 +1,131 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int n = in.nextInt();
+        int[] arr = ai(n);
+        Arrays.sort(arr);
+        long [] sums = new long [5001];
+        sums[0]++;
+        long sum = 0;
+        int mod = 998244353;
+        for (int i = 0; i < n; i++) {
+            long [] temp = Arrays.copyOf(sums, sums.length);
+            for (int j = 0; j < sums.length; j++) {
+                if(sums[j] > 0){
+                    if(j < arr[i]){
+                        sum = (sum + sums[j]*arr[i])%mod;
+                    } else{
+                        sum = (sum + (j + arr[i] + 1)/2 * sums[j])%mod;
+                    }
+                    temp[j + arr[i]] += sums[j]%mod;
+                }
+            }
+            sums = temp;
+        }
+        System.out.println(sum);
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U>  {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(!(obj instanceof Pair<?,?>)) return false;
+            Pair<T, U> pair = (Pair<T, U>) obj;
+            return f.equals(pair.f) && s.equals(pair.s);
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
