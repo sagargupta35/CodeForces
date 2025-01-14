b765063c8e730d7026cdc8994b6cd33d35f4b34b
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2025-01-14 13:29:36 +0530

    Create Sorting_By_Multiplication.java
    problem 4 - 14-01-2025 - greedy and dp

---
+++ Sorting_By_Multiplication.java
@@ -0,0 +1,135 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int[] arr = ai(n);
+            if(n == 1){
+                System.out.println(0);
+                continue;
+            }
+            int [] neg = new int [n];
+            neg[0]++;
+            for (int i = 1; i < n; i++) {
+                if(arr[i] < arr[i-1]) neg[i] = neg[i-1];
+                else neg[i] = neg[i-1]+1;
+            }
+            int [] pos = new int [n];
+            for (int i = n - 2; i >= 0; i--) {
+                if(arr[i] < arr[i+1]) pos[i] = pos[i+1];
+                else pos[i] = pos[i+1]+1;
+            }
+            int min = neg[n-1];
+            for (int i = n-2; i >= 0; i--) {
+                min = Math.min(neg[i] + pos[i+1], min);
+            }
+            min = Math.min(min, pos[0]);
+            System.out.println(min);
+        }
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U>  {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(!(obj instanceof Pair<?,?>)) return false;
+            Pair<T, U> pair = (Pair<T, U>) obj;
+            return f.equals(pair.f) && s.equals(pair.s);
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
