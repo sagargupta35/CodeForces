f4ec1cca130f2807efebed4586e5aada50c57987
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2025-01-18 10:22:30 +0530

    Create Lisa_and_the_Martians.java
    problem 1 - 18-01-2025 - bitmasking and greedy

---
+++ Lisa_and_the_Martians.java
@@ -0,0 +1,140 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            Pair<Integer, Long> [] arr = new Pair[n];
+            Arrays.setAll(arr, i -> new Pair<>(i, in.nextLong()));
+            Arrays.sort(arr, Comparator.comparingLong(p -> p.s));
+            int i = -1, j = -1;
+            long xr = -1;
+            long max = -1;
+            for (int l = 0; l < n - 1; l++) {
+                long res = 0;
+                long x = 0;
+                long pow = 1;
+                int cnt = 0;
+                while (cnt < k){
+                    if((arr[l].s&pow) == (arr[l+1].s&pow)){
+                        res ^= pow;
+                        if((arr[l].s&pow) == 0) x ^= pow;
+                    }
+                    cnt++;
+                    pow *= 2;
+                }
+                if(res > max){
+                    max = res;
+                    xr = x;
+                    i = arr[l].f+1; j = arr[l+1].f+1;
+                }
+            }
+            System.out.println(i + " " + j + " " + xr);
+        }
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U>  {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(!(obj instanceof Pair<?,?>)) return false;
+            Pair<T, U> pair = (Pair<T, U>) obj;
+            return f.equals(pair.f) && s.equals(pair.s);
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
