1c50272e58f743e6b731bf0b0d008a1497265134
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2025-01-20 11:23:08 +0530

    Create Graph_Composition.java
    problem 1 - 20-01-2025 - dsu

---
+++ Graph_Composition.java
@@ -0,0 +1,191 @@
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int m1 = in.nextInt();
+            int m2 = in.nextInt();
+            List<Integer> [] adj1 = new ArrayList [n];
+            List<Integer> [] adj2 = new ArrayList [n];
+            Arrays.setAll(adj1, i -> new ArrayList<>());
+            Arrays.setAll(adj2, i -> new ArrayList<>());
+            for (int i = 0; i < m1; i++) {
+                int u = in.nextInt()-1;
+                int v = in.nextInt()-1;
+                adj1[u].add(v);;
+                adj1[v].add(u);;
+            }
+            for (int i = 0; i < m2; i++) {
+                int u = in.nextInt()-1;
+                int v = in.nextInt()-1;
+                adj2[u].add(v);
+                adj2[v].add(u);
+            }
+            Dsu dsu = new Dsu(n);
+            Dsu d2 = new Dsu(n);
+            for (int i = 0; i < n; i++) {
+                for (var j : adj2[i]) {
+                    dsu.union(i, j);
+                }
+            }
+            int sum = 0, ex = 0;
+            for (int i = 0; i < n; i++) {
+                for (var j : adj1[i]) {
+                    if(dsu.findPar(i) != dsu.findPar(j)) sum++;
+                    else{
+                        d2.union(i, j);
+                    }
+                }
+            }
+            for(int i = 0; i<n; i++){
+                for(int j : adj2[i]){
+                    if(d2.findPar(i) != d2.findPar(j)){
+                        ex++;
+                        d2.union(i, j);
+                    }
+                }
+            }
+            System.out.println(sum/2 + ex);
+        }
+    }
+
+    static class Dsu{
+
+        List<Integer> size = new ArrayList<>();
+        List<Integer> parent = new ArrayList<>();
+
+        public Dsu(int n) {
+            for (int i = 0; i < n; i++) {
+                size.add(1);
+                parent.add(i);
+            }
+        }
+
+        int findPar(int u){
+            if(parent.get(u) == u) return u;
+            int par = findPar(parent.get(u));
+            parent.set(u, par);
+            return par;
+        }
+
+        void union(int u, int v){
+            int up = findPar(u);
+            int vp = findPar(v);
+            if(up == vp) return;
+            if(size.get(up) < size.get(vp)){
+                parent.set(up, vp);
+                size.set(vp, size.get(up) + size.get(vp));
+            } else{
+                parent.set(vp, up);
+                size.set(up, size.get(up) + size.get(vp));
+            }
+        }
+
+    }
+
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U>  {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(!(obj instanceof Pair<?,?>)) return false;
+            Pair<T, U> pair = (Pair<T, U>) obj;
+            return f.equals(pair.f) && s.equals(pair.s);
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
