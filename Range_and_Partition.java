f9a3baa665301ae83fac44edb3fc300abad33259
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2025-02-05 11:22:08 +0530

    Create Range_and_Partition.java
    problem 2 - 05-02-2025 - two pointers and greedy

---
+++ Range_and_Partition.java
@@ -0,0 +1,157 @@
+
+import java.io.*;
+import java.util.*;
+
+public class CodeChef3 {
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int[] arr = ai(n);
+            int x = 0, y = Integer.MAX_VALUE;
+            int [] cp = Arrays.copyOf(arr, n);
+            Arrays.sort(cp);
+            int el = (k+n+1)/2;
+            for (int i = 0; i <= n - el; i++) {
+                int diff = cp[i+el-1] - cp[i];
+                if(diff < y - x){
+                    x = cp[i];
+                    y =cp[i+el-1];
+                }
+            }
+            int [] pre = new int [n];
+            for (int i = 0; i < n; i++) {
+                if(x <= arr[i] && arr[i] <= y) pre[i]++;
+                else pre[i]--;
+                if(i > 0) pre[i] += pre[i-1];
+            }
+            List<Pair<Integer, Integer>> res = new ArrayList<>(k);
+            int next = 1, prev = 0;
+            for (int i = 0; i < n && res.size() < k; i++) {
+                if(pre[i] == next){
+                    res.add(new Pair<>(prev+1, i+1));
+                    next++;
+                    prev = i+1;
+                }
+            }
+            res.get(k-1).s = n;
+            System.out.println(x + " " + y);
+            for(var p : res){
+                System.out.println((p));
+            }
+
+        }
+    }
+
+    static void reverse(int [] arr){
+        int n = arr.length;
+        for (int i = 0; i < n/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n-i-1];
+            arr[n-i-1] = temp;
+        }
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static class Pair<T, U>  {
+        T f;
+        U s;
+
+        public Pair(T first, U sec) {
+            this.f = first;
+            this.s = sec;
+        }
+
+        @Override
+        public String toString() {
+            return f + " " + s;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(!(obj instanceof Pair<?,?>)) return false;
+            Pair<T, U> pair = (Pair<T, U>) obj;
+            return f.equals(pair.f) && s.equals(pair.s);
+        }
+    }
+
+    static int mod = (int)1e9 + 7;
+
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static FastReader in = new FastReader();
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return java.lang.Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
